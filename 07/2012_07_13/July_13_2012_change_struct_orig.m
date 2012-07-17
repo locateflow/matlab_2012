c77fb3e6bae52c6a13cef385c2f37d4a34d64542
Eathan <Eathan@EATHAN-PC>
2012-07-17 16:33:40 -0400

    Still continuing to explore HT99.  I had a new idea. Take each data point and look for similar ones in the future. Using a couple parameters.  If sequences of similar data points follow then group them into elements.

--- 07/2012_07_13/July_13_2012_change_struct_orig.m
+++ 07/2012_07_13/July_13_2012_change_struct_orig.m
@@ -1,13 +1,14 @@
 filesFolder = 'Media\Sound\HT99'
 % Get the directory list of the input folder.
 fileList = dir(filesFolder);
+%%
 % Skip the nonsense at the beginning.
 fileList = fileList(3:end);
 % Change the structure to a cell.
 fileListCell = struct2cell(fileList);
 % Nameslist only contains the wav file names.
 namesList = fileListCell(1,:);
-namesList = fliplr(namesList);
+% namesList = fliplr(namesList);
 
 %% fields: original, amplitude, frequency.
 structr = HT99;
@@ -19,4 +20,7 @@ wv = wavesc(wv);
 structr(i).original = (wv(:,1));
 end
 %%stru
-imageButcherbird(structr,'raw');title('HT99');set(gca,'clim',[800,2000])
+imageButcherbird(structr,'raw');title('HT99');set(gca,'clim',[800,2000]); ylim([0 50])
+%%
+imageButcherbird(HT99,'raw');title('HT99');set(gca,'clim',[800,2000]); ylim([0 50])
+
--- DailyCommandsLog.m
+++ DailyCommandsLog.m
@@ -1,9 +1,9 @@
-% looked for ht99 hand sorted in google desktop search, no luck.
-% look at the data as sorted by the algorithm.
-load 2011/10/10_13_2011 HT99 HT993
-%%
+load C:\Users\Eathan\Documents\MATLAB\2011\10\10_13_2011.mat HT99
 
-imageButcherbird(HT99,'subsorting');title('HT99');set(gca,'clim',[800,2000])
-imageButcherbird(HT993,'subsorting');title('HT993');set(gca,'clim',[800,2000])
+% I also used C:\Users\Eathan\Documents\MATLAB\2012\07\2012_07_13\July_13_2012_change_struct_orig.m
 %%
-rearrange_image_rows
+imageButcherbird(structr,'subsorting');title('HT99');set(gca,'clim',[800,2000]); 
+%%
+imageButcherbird(HT99,'subsorting');title('HT99 old');set(gca,'clim',[800,2000]); 
+%%
+imageButcherbird(structr,'raw');title('HT99');set(gca,'clim',[800,2000]); 
