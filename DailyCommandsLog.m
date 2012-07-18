1c4942d3151acb59a0c20390e2f81f14b3f74475
Eathan <Eathan@EATHAN-PC>
2012-07-18 17:48:53 -0400

    Trying a different visualization for repeated "elements" using raw features that occur together.

--- DailyCommandsLog.m
+++ DailyCommandsLog.m
@@ -1,9 +1,31 @@
+wav = wavread('Media\HT99_small_sample.wav');
+%%
 load C:\Users\Eathan\Documents\MATLAB\2011\10\10_13_2011.mat HT99
+structr = HT99;
+%%
+amp = structr(1).consolidated.ampAligned;
+freq = structr(1).consolidated.freqAligned;
+combo = zeros(size(amp));
+%%
+% Go through the file and see where certain amp/freq combos recur.
+for i = 1:length(amp(1,:))
+    for j = 1:length(amp(:,1))
+        fq = freq(i,j);
+f = (abs(amp - amp(i,j)) < .1 & abs(freq - fq) < 10);
 
-% I also used C:\Users\Eathan\Documents\MATLAB\2012\07\2012_07_13\July_13_2012_change_struct_orig.m
+imagesc(f);
+colormap('bone')
+
+title([i, ' ', j])
+pause
+    end
+    
+end
 %%
-imageButcherbird(structr,'subsorting');title('HT99 new');set(gca,'clim',[800,2000]); 
-%%
-imageButcherbird(HT99,'subsorting');title('HT99 old');set(gca,'clim',[800,2000]); 
-%%
-imageButcherbird(structr,'raw');title('HT99');set(gca,'clim',[800,2000]); 
+am = reshape(amp, 1, 161*2500);
+fre = reshape(freq, 1, 161*2500);
+
+scatter(am, fre, '.');
+
+
+
