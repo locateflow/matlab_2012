3ce6946dacfb6564c658fb1a091e72ae3c00034b
Eathan <Eathan@EATHAN-PC>
2012-07-18 16:09:14 -0400

    Used basically the same dialy command log as yesterday. Now pushing to github.  Starting a new task, so I will clear the log after pushing.

--- DailyCommandsLog.m
+++ DailyCommandsLog.m
@@ -2,7 +2,7 @@ load C:\Users\Eathan\Documents\MATLAB\2011\10\10_13_2011.mat HT99
 
 % I also used C:\Users\Eathan\Documents\MATLAB\2012\07\2012_07_13\July_13_2012_change_struct_orig.m
 %%
-imageButcherbird(structr,'subsorting');title('HT99');set(gca,'clim',[800,2000]); 
+imageButcherbird(structr,'subsorting');title('HT99 new');set(gca,'clim',[800,2000]); 
 %%
 imageButcherbird(HT99,'subsorting');title('HT99 old');set(gca,'clim',[800,2000]); 
 %%
