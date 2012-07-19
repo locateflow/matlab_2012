0d279736b321f78b8b5da93921530bc79cd6d097
Eathan <Eathan@EATHAN-PC>
2012-07-19 10:53:55 -0400

    Changed the daily commands log (DCL) to a previous day. Added content from July_13_2012_change_struct_orig.m because the code was being used together.
    July_13... added the unfiltered original sounds to the structure.
    This daily log allows me to quickly load up figures so I can
    categorize elements of HT77 and enter them into the spreadsheet.

--- DailyCommandsLog.m
+++ DailyCommandsLog.m
@@ -1,46 +1,26 @@
-wav = wavread('Media\HT99_small_sample.wav');
-%%
 load C:\Users\Eathan\Documents\MATLAB\2011\10\10_13_2011.mat HT99
+
+filesFolder = 'Media\Sound\HT99'
+% Get the directory list of the input folder.
+fileList = dir(filesFolder);
+%%
+% Skip the nonsense at the beginning.
+fileList = fileList(3:end);
+% Change the structure to a cell.
+fileListCell = struct2cell(fileList);
+% Nameslist only contains the wav file names.
+namesList = fileListCell(1,:);
+%% fields: original, amplitude, frequency.
 structr = HT99;
-%%
-amp = structr(1).consolidated.ampAligned;
-freq = structr(1).consolidated.freqAligned;
-combo = zeros(size(amp));
-%%
-% Go through the file and see where certain amp/freq combos recur.
-for i = 1:length(amp(1,:))
-    for j = 1:length(amp(:,1))
-        fq = freq(i,j);
-f = (abs(amp - amp(i,j)) < .1 & abs(freq - fq) < 10);
-
-imagesc(f);
-colormap('bone')
-
-title([i, ' ', j])
-pause
-    end
-    
+for i = 1:length(namesList)
+structr(i).fileLoc = [filesFolder,'\',cell2mat(namesList(i))];
+wv = wavread(structr(i).fileLoc);
+% Regularize the data so that the vector's range extends from -1 to 1.
+wv = wavesc(wv);
+structr(i).original = (wv(:,1));
 end
 %%
-% Go through the file and see where certain amp/freq combos recur.
-% for am = .01:.01:1
-    for fre  = 800:1:2000
-% f = (abs(amp - am) < .1 & abs(freq - fre) < 10);
-f = (abs(freq - fre) < 10);
-
-imagesc(f);
-colormap('bone')
-
-title(num2str(fre))
-pause
-    end
-    
-% end
-%%
-am = reshape(amp, 1, 161*2500);
-fre = reshape(freq, 1, 161*2500);
-
-scatter(am, fre, '.');
-
-
-
+imageButcherbird(structr,'subsorting');title('HT99 new');set(gca,'clim',[800,2000]); 
+imageButcherbird(HT99,'subsorting');title('HT99 old');set(gca,'clim',[800,2000]); 
+imageButcherbird(structr,'raw');title('HT99 new');set(gca,'clim',[800,2000]); 
+imageButcherbird(HT99,'raw');title('HT99 old');set(gca,'clim',[800,2000]);
