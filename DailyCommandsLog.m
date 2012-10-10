0ad136a3dc240861fca31b43fc7548bec7fbdd15
Eathan <Eathan@EATHAN-PC>
2012-10-10 15:51:24 -0400

    commit now just in case.

--- DailyCommandsLog.m
+++ DailyCommandsLog.m
@@ -1,3 +1,17 @@
 load('C:\Users\Eathan\Documents\MATLAB\2012\09\ElmentStructureForAllSixBirds.mat')
 %%
-viewElementDistributionHistogramsPDF(Beasley, 'Beasley - IPI Distribution (elements)', 'BeasleyIPIForElementDistribution')
+birdNames = {'Bayliss', 'Beasley', 'HT77', 'HT99', 'Lumsdaine', 'Powys'};
+%%
+for i = 1:length(birdNames)
+    allElementFreqs = [allElementFreqs; findElementDistributionPatternHelper1(eval(cell2mat(birdNames(i))),birdNames(i))]
+end
+
+%%
+commandString = 'inputMarkovMat = PowysPhraseMarkov; elementMat = Powys; PowysPhraseBasedElemntModel = makePhraseMarkovElementModelFromMarkovMat(inputMarkovMat, elementMat); compareElementDistributionsPDF(Powys, PowysPhraseBasedElemntModel, "Powys", "Phrase Based Markov Model", "PowysPhraseBasedMarkovModel");'
+%%
+eval(commandString);
+%%
+inputMarkovMat = PowysPhraseMarkov;   
+elementMat = Powys;
+PowysPhraseBasedElementModel = makePhraseMarkovElementModelFromMarkovMat(inputMarkovMat, elementMat);
+compareElementDistributionsPDF(Powys, PowysPhraseBasedElementModel, 'Powys', 'Phrase Based Markov Model', 'PowysPhraseBasedMarkovModel');
