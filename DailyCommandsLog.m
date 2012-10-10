1eaa59ea94f45807e1f871f1d0392e608e643e20
Eathan <Eathan@EATHAN-PC>
2012-10-10 16:09:25 -0400

    Make phrase-based markov model ipi histograms.

--- DailyCommandsLog.m
+++ DailyCommandsLog.m
@@ -1,8 +1,2 @@
-load('C:\Users\Eathan\Documents\MATLAB\2012\09\PhraseMarkovMatsForAllSixBirds.mat')
-load('C:\Users\Eathan\Documents\MATLAB\2012\09\ElmentStructureForAllSixBirds.mat')
-
 %%
-% used find and replace for all six birds then saved data file.
-inputMarkovMat = BaylissPhraseMarkov;   
-elementMat = Bayliss;
-BaylissPhraseBasedElementModel = makePhraseMarkovElementModelFromMarkovMat(inputMarkovMat, elementMat);
+viewPhraseDistributionHistogramsPDF(BaylissPhraseBasedElementModel, 'Bayliss Phrase-based Markov Model  - IPI Distribution', 'BaylissIPIDistributionPhraseMarkov')
