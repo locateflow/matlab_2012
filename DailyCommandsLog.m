5a7910bfea5378b527f3e7e2a327b438ef62b315
Eathan <Eathan@EATHAN-PC>
2012-10-25 10:44:25 -0400

    Intermediate - understanding KS Statistic function in Matlab.

--- DailyCommandsLog.m
+++ DailyCommandsLog.m
@@ -4,5 +4,19 @@ load('C:\Users\Eathan\Documents\MATLAB\2012\09\ElmentStructureForAllSixBirds.mat
 BaylissRandomizedPhraseMarkovModel = randomizePhraseMarkovModel(BaylissPhraseBasedElementModel);
 phraseModel = BaylissRandomizedPhraseMarkovModel;
 originalElementStructure = BaylissPhraseBasedElementModel;
-BaylissrandomizedMarkovPhraseModelwElementStructure = elementStructureFromPhraseModel(phraseModel, originalElementStructure)
+%%
+figure
+allDiffs = viewPhraseDistributionHistograms(Powys, 'title');
+figure
+allDiffs2 = viewPhraseDistributionHistograms(PowysPhraseBasedElementModel, 'title');
 
+figure
+[h,p,k] = kstest2(allDiffs,allDiffs2)
+x = allDiffs;
+y = allDiffs2;
+F1 = cdfplot(x);
+hold on
+F2 = cdfplot(y)
+set(F1,'LineWidth',2,'Color','r')
+set(F2,'LineWidth',2)
+legend([F1 F2],'F1(x)','F2(x)','Location','NW')
