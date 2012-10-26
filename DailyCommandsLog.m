4c4f69550aacee0bf4184bc3ce112050b3133cc4
Eathan <Eathan@EATHAN-PC>
2012-10-26 10:31:45 -0400

    Consolidate IPI information.

--- DailyCommandsLog.m
+++ DailyCommandsLog.m
@@ -1,22 +1,13 @@
 load('C:\Users\Eathan\Documents\MATLAB\2012\09\PhraseBasedElementMarkovModelForAllSixBirds.mat')
 load('C:\Users\Eathan\Documents\MATLAB\2012\09\ElmentStructureForAllSixBirds.mat')
 %%
-BaylissRandomizedPhraseMarkovModel = randomizePhraseMarkovModel(BaylissPhraseBasedElementModel);
-phraseModel = BaylissRandomizedPhraseMarkovModel;
-originalElementStructure = BaylissPhraseBasedElementModel;
+% Do find and "replace all" for all six birds...
+PowysElementIPIs = getElementIPIs(Powys);
+PowysPhraselModelElementIPIs = getElementIPIs(PowysPhraseBasedElementModel);
 %%
-figure
-allDiffs = viewPhraseDistributionHistograms(Powys, 'title');
-figure
-allDiffs2 = viewPhraseDistributionHistograms(PowysPhraseBasedElementModel, 'title');
-
-figure
-[h,p,k] = kstest2(allDiffs,allDiffs2)
-x = allDiffs;
-y = allDiffs2;
-F1 = cdfplot(x);
-hold on
-F2 = cdfplot(y)
-set(F1,'LineWidth',2,'Color','r')
-set(F2,'LineWidth',2)
-legend([F1 F2],'F1(x)','F2(x)','Location','NW')
+save 2012\09\ElementIPIsAllSixBirds
+%%
+BaylissIPIs.elements.original = BaylissElementIPIs;
+BaylissIPIs.elements.phraseMarkovModel = BaylissElementIPIs;
+BaylissIPIs.phrases.original = BaylissElementIPIs;
+BaylissIPIs.phrases.phraseMarkovModel = BaylissPhraselModelElementIPIs;
