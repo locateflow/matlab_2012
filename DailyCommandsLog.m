7be46f620795ab9b92890984ba2c194cbd92d90c
Eathan <Eathan@EATHAN-PC>
2012-10-26 14:15:49 -0400

    Last commit was a mistake. This content applies to last commit message.

--- DailyCommandsLog.m
+++ DailyCommandsLog.m
@@ -1,27 +1,27 @@
-function view3PhraseDistributionsToComparePDF(elementMat1, elementMat2, elementMat3, tit1, tit2, tit3, filename)
-subMat1 = elementMat1(:,1);
-subMat2 = elementMat2(:,1);
-subMat3 = elementMat3(:,1);
-u = unique(subMat1);
-numPhrases = length(u(~isnan(u)))
+%%
+% Do find and "replace all" for all six birds...
+birdNames = {'BaylissIPIs', 'BeasleyIPIs', 'HT77IPIs', 'HT99IPIs', 'LumsdaineIPIs', 'PowysIPIs'};
+for i = 1:length(birdNames)
 
-for i = 1:numPhrases  
-    subplot(131)
-    viewElements1 = subMat1 == u(i);
-    imagesc(viewElements1*-1);colormap(bone); 
-    title([tit1, ' - Phrase (', num2str(u(i)), ') - ', num2str(i), '/', num2str(numPhrases)]);
+eval(['thisStruct = ', (cell2mat(birdNames(i))), ';'])
+birdName = cell2mat(birdNames(i));
+figure
+subplot(4,1,1:2)
+a = thisStruct.phrases.original.allDiffsNormalized;
+b = thisStruct.phrases.phraseMarkovModel.allDiffsNormalized;
+[h,p,k] = kstest2(a, b);
+t = ['h = ', num2str(h), char(10), 'p = ', num2str(p), char(10), 'k = ', num2str(k)];
 
-    subplot(132)
-    viewElements2 = subMat2 == u(i);
-    imagesc(viewElements2*-1);colormap(bone); 
-    title(tit2)    
-    subplot(133)
-    viewElements3 = subMat3 == u(i);
-    imagesc(viewElements3*-1);colormap(bone); 
-    title(tit3)
-%     filename = ['C:\Users\Eathan\Documents\MATLAB\2012\09\Figures\BarcodesForPhrases\', title, 'Phrase', num2str(i), 'of', num2str(numPhrases), '.bmp'];
-%     saveas(gcf, ['C:\Users\Eathan\Documents\MATLAB\2012\09\Figures\BarcodesForPhrases\PDF\', tit, 'Phrase', sprintf('%02d',i), 'of', num2str(numPhrases), '.pdf']) 
-    saveas(gcf, ['C:\Users\Eathan\Documents\MATLAB\2012\09\Figures\ComparePhraseDistributionModels\', filename, 'Phrase', sprintf('%02d',i), 'of', num2str(numPhrases), '.pdf']) 
+hist(a);
+title([birdName, char(10) ' IPI distribution (phrases, original data)'])
+axisLim = get(gca, 'xlim');
+set(gca, 'xtick', []);
 
-% pause
+text(max(xlim)*0.5, max(ylim)*0.8, t)
+subplot(4,1,3:4)
+hist(b);
+title([birdName, char(10) ' IPI distribution (phrases, phrase markov)'])
+
+set(gca, 'xlim', axisLim);
+saveas(gcf, ['C:\Users\Eathan\Documents\MATLAB\2012\10\Figures\kstests\', birdName, 'OrigVsPhraseMarkov_phrases.pdf']) 
 end
