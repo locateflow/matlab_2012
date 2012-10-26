dc8ef1e2728697b84bf34d9774f3d8ab4605b783
Eathan <Eathan@EATHAN-PC>
2012-10-26 14:14:37 -0400

    Original v Phrase Markov (compare phrase distributions)

--- DailyCommandsLog.m
+++ DailyCommandsLog.m
@@ -1,41 +1,27 @@
-load('C:\Users\Eathan\Documents\MATLAB\2012\09\PhraseBasedElementMarkovModelForAllSixBirds.mat')
-load('C:\Users\Eathan\Documents\MATLAB\2012\09\ElmentStructureForAllSixBirds.mat')
-%%
-% Do find and "replace all" for all six birds...
-PowysElementIPIs = getElementIPIs(Powys);
-PowysPhraselModelElementIPIs = getElementIPIs(PowysPhraseBasedElementModel);
-%%
-save 2012\09\ElementIPIsAllSixBirds
-%%
-% Do find and "replace all" for all six birds...
-LumsdaineIPIs.elements.original = LumsdaineElementIPIs;
-LumsdaineIPIs.elements.phraseMarkovModel = LumsdainePhraselModelElementIPIs;
-LumsdaineIPIs.phrases.original = LumsdainePhraseIPIs;
-LumsdaineIPIs.phrases.phraseMarkovModel = LumsdainePhraselModelPhraseIPIs;
-%%
-save 2012\09\allIPIsAllSixBirds
-%%
-% Do find and "replace all" for all six birds...
-thisStruct = BaylissIPIs;
-birdName = 'Bayliss';
-figure
-subplot(4,1,1:2)
-a = thisStruct.elements.original.allDiffsNormalized;
-b = thisStruct.elements.phraseMarkovModel.allDiffsNormalized;
-[h,p,k] = kstest2(a, b);
-t = ['h = ', num2str(h), char(10), 'p = ', num2str(p), char(10), 'k = ', num2str(k)];
+function view3PhraseDistributionsToComparePDF(elementMat1, elementMat2, elementMat3, tit1, tit2, tit3, filename)
+subMat1 = elementMat1(:,1);
+subMat2 = elementMat2(:,1);
+subMat3 = elementMat3(:,1);
+u = unique(subMat1);
+numPhrases = length(u(~isnan(u)))
 
-hist(a);
-title([birdName, char(10) ' IPI distribution (elements, original data)'])
-axisLim = get(gca, 'xlim');
-set(gca, 'xtick', []);
+for i = 1:numPhrases  
+    subplot(131)
+    viewElements1 = subMat1 == u(i);
+    imagesc(viewElements1*-1);colormap(bone); 
+    title([tit1, ' - Phrase (', num2str(u(i)), ') - ', num2str(i), '/', num2str(numPhrases)]);
 
-text(max(xlim)*0.5, max(ylim)*0.8, t)
-subplot(4,1,3:4)
-hist(b);
-title([birdName, char(10) ' IPI distribution (elements, phrase markov)'])
+    subplot(132)
+    viewElements2 = subMat2 == u(i);
+    imagesc(viewElements2*-1);colormap(bone); 
+    title(tit2)    
+    subplot(133)
+    viewElements3 = subMat3 == u(i);
+    imagesc(viewElements3*-1);colormap(bone); 
+    title(tit3)
+%     filename = ['C:\Users\Eathan\Documents\MATLAB\2012\09\Figures\BarcodesForPhrases\', title, 'Phrase', num2str(i), 'of', num2str(numPhrases), '.bmp'];
+%     saveas(gcf, ['C:\Users\Eathan\Documents\MATLAB\2012\09\Figures\BarcodesForPhrases\PDF\', tit, 'Phrase', sprintf('%02d',i), 'of', num2str(numPhrases), '.pdf']) 
+    saveas(gcf, ['C:\Users\Eathan\Documents\MATLAB\2012\09\Figures\ComparePhraseDistributionModels\', filename, 'Phrase', sprintf('%02d',i), 'of', num2str(numPhrases), '.pdf']) 
 
-set(gca, 'xlim', axisLim);
-saveas(gcf, ['C:\Users\Eathan\Documents\MATLAB\2012\10\Figures\kstests\', birdName, 'OrigVsPhraseMarkov_elements.pdf']) 
-
-
+% pause
+end
