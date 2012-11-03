b651709c5caa620c066bd30799eaa2c253447d99
Eathan <Eathan@EATHAN-PC>
2012-11-02 22:34:28 -0400

    t test between medians of distribution IPI element vs phrase

--- DailyCommandsLog.m
+++ DailyCommandsLog.m
@@ -1,22 +1,31 @@
-load('C:\Users\Eathan\Documents\MATLAB\2012\09\PhraseBasedElementMarkovModelForAllSixBirds.mat')
-load('C:\Users\Eathan\Documents\MATLAB\2012\09\ElmentStructureForAllSixBirds.mat')
-%%
-BaylissRandomizedPhraseMarkovModel = randomizePhraseMarkovModel(BaylissPhraseBasedElementModel);
-% phraseModel = BaylissRandomizedPhraseMarkovModel;
-% originalElementStructure = BaylissPhraseBasedElementModel;
-% BaylissrandomizedMarkovPhraseModelwElementStructure = elementStructureFromPhraseModel(phraseModel, originalElementStructure)
-%%
+diffScores = zeros(6,1);
+birdNames = {'Bayliss', 'Beasley', 'HT77', 'HT99', 'Lumsdaine', 'Powys'};
+for i = 1:length(birdNames)
 figure
-imagesc(BaylissRandomizedPhraseMarkovModel)
-figure
-imagesc(BaylissPhraseBasedElementModel)
-figure
-Bayliss
-%%
-modelElementMat = BaylissRandomizedPhraseMarkovModel;
-originalElementMat = BaylissPhraseBasedElementModel;
-birdName = 'Bayliss';
-altTitle = 'From Randomized Markov';
-filename = 'BaylissCompareRandomizedMarkov';
+% birdName = 'Beasley';
+birdName = (cell2mat(birdNames(i)))
+subplot(211)
+data1 = eval([birdName, 'IPIs.elements.original.allDiffsNormalized;'])
+m1 = mode(data1)
+% sum(BeasleyIPIs.elements.original.allDiffsNormalized==m1)
+hist(data1, 0:0.5:5)
+title([birdName, char(10), 'element IPI distribution'])
 
-compareElementDistributionsPDF(modelElementMat, originalElementMat, birdName, altTitle, filename)
+subplot(212)
+data2 = eval([birdName, 'IPIs.phrases.original.allDiffsNormalized;'])
+m2 = mode(data2)
+% sum(BeasleyIPIs.phrases.original.allDiffsNormalized==m2)
+hist(data2, 0:0.5:5)
+d = m1-m2;
+title(['phrase IPI distribution ', 'm1 - m2 = ', num2str(d)])
+diffScores(i) = d;
+end
+%%
+MeanDiff = sum(diffScores)/6
+SS = sum((MeanDiff - diffScores).^2);
+s = (SS/5)^.5;
+t = MeanDiff/(s/(6^.5))
+[h, p] = ttest(diffScores, 0, .05, 'right')
+
+% for medians, the p value is .0479
+% for medians, fail to reject.  the p value is 0.6741
