c7ed5a468db63559d9b38b9a4742b7aec1525d6f
Eathan <Eathan@EATHAN-PC>
2012-11-05 10:26:39 -0500

    Comparing peak frequencies of butcherbirds.
    Signed-off-by: Eathan <Eathan@EATHAN-PC>

--- DailyCommandsLog.m
+++ DailyCommandsLog.m
@@ -1,31 +1,14 @@
-diffScores = zeros(6,1);
-birdNames = {'Bayliss', 'Beasley', 'HT77', 'HT99', 'Lumsdaine', 'Powys'};
-for i = 1:length(birdNames)
-figure
-% birdName = 'Beasley';
-birdName = (cell2mat(birdNames(i)))
-subplot(211)
-data1 = eval([birdName, 'IPIs.elements.original.allDiffsNormalized;'])
-m1 = mode(data1)
-% sum(BeasleyIPIs.elements.original.allDiffsNormalized==m1)
-hist(data1, 0:0.5:5)
-title([birdName, char(10), 'element IPI distribution'])
+Fs = 44100;
+% t = 0:1/Fs:.3;
+% x=cos(2*pi*t*200)+randn(size(t));
+x = Bayliss(3).original
+% Hs=spectrum.periodogram;
+Hs=spectrum.eigenvector;
 
-subplot(212)
-data2 = eval([birdName, 'IPIs.phrases.original.allDiffsNormalized;'])
-m2 = mode(data2)
-% sum(BeasleyIPIs.phrases.original.allDiffsNormalized==m2)
-hist(data2, 0:0.5:5)
-d = m1-m2;
-title(['phrase IPI distribution ', 'm1 - m2 = ', num2str(d)])
-diffScores(i) = d;
-end
+psd(Hs,x,'Fs',Fs)
+
 %%
-MeanDiff = sum(diffScores)/6
-SS = sum((MeanDiff - diffScores).^2);
-s = (SS/5)^.5;
-t = MeanDiff/(s/(6^.5))
-[h, p] = ttest(diffScores, 0, .05, 'right')
-
-% for medians, the p value is .0479
-% for medians, fail to reject.  the p value is 0.6741
+% Bayliss 1 (kHz) highest peak, second highest
+% 1.38, 1.713
+% 2 - 1.414, 2.21
+% 3) 1.351, 1.55
