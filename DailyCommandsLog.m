f85059a809c090d3ecffba3a9e79bc004a736b2c
Eathan <Eathan@EATHAN-PC>
2012-11-07 15:25:27 -0500

    Make six shuffled phrase markov models.  Save them.

--- DailyCommandsLog.m
+++ DailyCommandsLog.m
@@ -1,27 +1,11 @@
+load('C:\Users\Eathan\Documents\MATLAB\2012\09\PhraseBasedElementMarkovModelForAllSixBirds.mat')
+load('C:\Users\Eathan\Documents\MATLAB\2012\09\ElmentStructureForAllSixBirds.mat')
 %%
-% Do find and "replace all" for all six birds...
-birdNames = {'BaylissIPIs', 'BeasleyIPIs', 'HT77IPIs', 'HT99IPIs', 'LumsdaineIPIs', 'PowysIPIs'};
-for i = 1:length(birdNames)
-
-eval(['thisStruct = ', (cell2mat(birdNames(i))), ';'])
-birdName = cell2mat(birdNames(i));
-figure
-subplot(4,1,1:2)
-a = thisStruct.phrases.original.allDiffsNormalized;
-b = thisStruct.phrases.phraseMarkovModel.allDiffsNormalized;
-[h,p,k] = kstest2(a, b);
-t = ['h = ', num2str(h), char(10), 'p = ', num2str(p), char(10), 'k = ', num2str(k)];
-
-hist(a);
-title([birdName, char(10) ' IPI distribution (phrases, original data)'])
-axisLim = get(gca, 'xlim');
-set(gca, 'xtick', []);
-
-text(max(xlim)*0.5, max(ylim)*0.8, t)
-subplot(4,1,3:4)
-hist(b);
-title([birdName, char(10) ' IPI distribution (phrases, phrase markov)'])
-
-set(gca, 'xlim', axisLim);
-saveas(gcf, ['C:\Users\Eathan\Documents\MATLAB\2012\10\Figures\kstests\', birdName, 'OrigVsPhraseMarkov_phrases.pdf']) 
-end
+BaylissRandomizedPhraseMarkovModel = randomizePhraseMarkovModel(BaylissPhraseBasedElementModel);
+BeasleyRandomizedPhraseMarkovModel = randomizePhraseMarkovModel(BeasleyPhraseBasedElementModel);
+HT77RandomizedPhraseMarkovModel = randomizePhraseMarkovModel(HT77PhraseBasedElementModel);
+HT99RandomizedPhraseMarkovModel = randomizePhraseMarkovModel(HT99PhraseBasedElementModel);
+LumsdaineRandomizedPhraseMarkovModel = randomizePhraseMarkovModel(LumsdainePhraseBasedElementModel);
+PowysRandomizedPhraseMarkovModel = randomizePhraseMarkovModel(PowysPhraseBasedElementModel);
+%%
+save 2012\09\PhraseBasedElementMarkovModelForAllSixBirds_Shuffled
