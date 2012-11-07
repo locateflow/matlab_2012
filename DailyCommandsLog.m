2579a237ddec928667c701683c6d6386dff46d0b
Eathan <Eathan@EATHAN-PC>
2012-11-07 16:00:09 -0500

    Added the IPIs for phrases and elements to BirdNameIPIs in the mat where those structures are stored.
    Signed-off-by: Eathan <Eathan@EATHAN-PC>

--- DailyCommandsLog.m
+++ DailyCommandsLog.m
@@ -1,11 +1,14 @@
-load('C:\Users\Eathan\Documents\MATLAB\2012\09\PhraseBasedElementMarkovModelForAllSixBirds.mat')
-load('C:\Users\Eathan\Documents\MATLAB\2012\09\ElmentStructureForAllSixBirds.mat')
+
+load 2012\09\PhraseBasedElementMarkovModelForAllSixBirds_Shuffled
 %%
-BaylissRandomizedPhraseMarkovModel = randomizePhraseMarkovModel(BaylissPhraseBasedElementModel);
-BeasleyRandomizedPhraseMarkovModel = randomizePhraseMarkovModel(BeasleyPhraseBasedElementModel);
-HT77RandomizedPhraseMarkovModel = randomizePhraseMarkovModel(HT77PhraseBasedElementModel);
-HT99RandomizedPhraseMarkovModel = randomizePhraseMarkovModel(HT99PhraseBasedElementModel);
-LumsdaineRandomizedPhraseMarkovModel = randomizePhraseMarkovModel(LumsdainePhraseBasedElementModel);
-PowysRandomizedPhraseMarkovModel = randomizePhraseMarkovModel(PowysPhraseBasedElementModel);
+% Do find and "replace all" for all six birds...
+PowysPhraselModel_shuffledElementIPIs = getElementIPIs(PowysRandomizedPhraseMarkovModel);
+PowysPhraselModel_shuffledPhraseIPIs = getPhraseIPIs(PowysRandomizedPhraseMarkovModel);
+
+PowysIPIs.elements.phraseMarkovModel_shuffled = PowysPhraselModel_shuffledElementIPIs;
+PowysIPIs.phrases.phraseMarkovModel_shuffled = PowysPhraselModel_shuffledPhraseIPIs;
 %%
-save 2012\09\PhraseBasedElementMarkovModelForAllSixBirds_Shuffled
+% delete all intermediate's and save only BirdNameIPIs
+save C:\Users\Eathan\Documents\MATLAB\2012\09\allIPIsAllSixBirds.mat
+%%
+
