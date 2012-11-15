f0109e9f123f1ccba181f288b6675934fe69c949
Eathan <Eathan@EATHAN-PC>
2012-11-15 15:49:27 -0500

    Exploring the changes in CV for elements and phrases (coefficient of variance) Including markov model and shuffled markov models.

--- DailyCommandsLog.m
+++ DailyCommandsLog.m
@@ -11,4 +11,42 @@ PowysIPIs.phrases.phraseMarkovModel_shuffled = PowysPhraselModel_shuffledPhraseI
 % delete all intermediate's and save only BirdNameIPIs
 save C:\Users\Eathan\Documents\MATLAB\2012\09\allIPIsAllSixBirds.mat
 %%
+% Do find and "replace all" for all six birds...
+%% Finding the coefficient of variance.
 
+birdName = 'Powys'
+
+std(PowysIPIs.elements.original.allDiffsNormalized)/mean(PowysIPIs.elements.original.allDiffsNormalized)
+std(PowysIPIs.phrases.original.allDiffsNormalized)/mean(PowysIPIs.phrases.original.allDiffsNormalized)
+% save 2012\10\AllBirdsCVElmentPhrase
+%%
+load C:\Users\Eathan\Documents\MATLAB\2012\09\allIPIsAllSixBirds.mat
+%%
+birdNames = {'Bayliss', 'Beasley', 'HT77', 'HT99', 'Lumsdaine', 'Powys'};
+
+for i = 1:6
+    
+thisStruct = eval([cell2mat(birdNames(i)), 'IPIs']);
+
+ele = thisStruct.elements.original.allDiffsNormalized;
+phr = thisStruct.phrases.original.allDiffsNormalized;
+allBirdsCVElementPhrase.original(1:2,i) = [std(ele)/mean(ele); std(phr)/mean(phr)];
+
+ele = thisStruct.elements.phraseMarkovModel.allDiffsNormalized;
+phr = thisStruct.phrases.phraseMarkovModel.allDiffsNormalized;
+allBirdsCVElementPhrase.phraseMarkovModel(1:2,i) = [std(ele)/mean(ele); std(phr)/mean(phr)];
+
+ele = thisStruct.elements.phraseMarkovModel_shuffled.allDiffsNormalized;
+phr = thisStruct.phrases.phraseMarkovModel_shuffled.allDiffsNormalized;
+allBirdsCVElementPhrase.phraseMarkovModel_shuffled(1:2,i) = [std(ele)/mean(ele); std(phr)/mean(phr)];
+
+end
+%%
+d1 = diff(allBirdsCVElementPhrase.original)
+[h, p] =  ttest(diff(allBirdsCVElementPhrase.original))
+d2 = diff(allBirdsCVElementPhrase.phraseMarkovModel);
+[h, p] =  ttest(diff(allBirdsCVElementPhrase.phraseMarkovModel))
+d3 = diff(allBirdsCVElementPhrase.phraseMarkovModel_shuffled);
+[h, p] =  ttest(diff(allBirdsCVElementPhrase.phraseMarkovModel_shuffled))
+d_all = [d1; d2; d3];
+[h, p] =  ttest(diff([diff(d_all([1,3],:));diff(d_all(1:2,:))]))
