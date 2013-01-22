8a48746e8bc7b83c09fc17cdb150eb3160f404b5
Eathan <Eathan@EATHAN-PC>
2013-01-22 11:23:51 -0500

    Make original plot again.

--- DailyCommandsLog.m
+++ DailyCommandsLog.m
@@ -1,12 +1,13 @@
-% Did find and replace for all birds...
-PowysRandomPhraseModel = makeRandomPhraseModelFromElementMat(Powys);
-% Have to add 2012\10 to the pathf
-PowysRandomIPIs.elements.phraseRandom = getElementIPIs(PowysRandomPhraseModel);
-PowysRandomIPIs.phrases.phraseRandom = getPhraseIPIs(PowysRandomPhraseModel);
+
 %%
-% after doing for all bids, saved the workspace
-% save 'C:\Users\Eathan\Documents\MATLAB\2013\01\2013_01_21\RandomizedPhrasesData_8_birds'
+clear all
+load('C:\Users\Eathan\Documents\MATLAB\2012\12\2012_12_07\AllBirdsCVElementPhrase.mat')
+load('C:\Users\Eathan\Documents\MATLAB\2013\01\2013_01_21\Lumsdaine2ElementMatrix.mat')
 %%
+
+Lumsdaine2.elements.original = Lumsdaine2IPIs.elements;
+Lumsdaine2.phrases.original = Lumsdaine2IPIs.phrases;
+Lumsdaine2IPIs = Lumsdaine2;%
 %%
 %
 %
@@ -16,25 +17,24 @@ birdNames = {'Bayliss', 'Beasley', 'HT77', 'HT99', 'Lumsdaine', 'Powys', 'Johnso
 
 for i = 1:8
     
-thisStruct = eval([cell2mat(birdNames(i)), 'RandomIPIs']);
+thisStruct = eval([cell2mat(birdNames(i)), 'IPIs']);
 
-ele = thisStruct.elements.phraseRandom.allDiffsNormalized;
-phr = thisStruct.phrases.phraseRandom.allDiffsNormalized;
-% I don't have to rest of the data right now but kept the format below the
-% same.
-allBirdsCVElementPhrase.phraseRandom(1:2,i) = [std(ele)/mean(ele); std(phr)/mean(phr)];
+ele = thisStruct.elements.original.allDiffsNormalized;
+phr = thisStruct.phrases.original.allDiffsNormalized;
+allBirdsCVElementPhrase.original(1:2,i) = [std(ele)/mean(ele); std(phr)/mean(phr)];
 
 end
 %%
 %%
-d1 = diff(allBirdsCVElementPhrase.phraseRandom)
-[h, p] =  signtest(diff(allBirdsCVElementPhrase.phraseRandom))
+d1 = diff(allBirdsCVElementPhrase.original)
+[h, p] =  signtest(diff(allBirdsCVElementPhrase.original))
 %%
 [h, p] =  signtest(d1(1:8))
 %%
 
 %% Here is the code for the example
-plot(allBirdsCVElementPhrase.phraseRandom, '-o')
+figure
+plot(allBirdsCVElementPhrase.original, '-o')
 
 xlim([0,3])
 set(gca,'xtick', [0:1:3])
