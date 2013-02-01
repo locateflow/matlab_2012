d587171ec8533d57f2b703693bbb796447c14ffd
Eathan <Eathan@EATHAN-PC>
2013-02-01 18:41:19 -0500

    Successfully made phrase models from Markov matrix.

--- DailyCommandsLog.m
+++ DailyCommandsLog.m
@@ -1,3 +1,7 @@
+load('C:\Users\Eathan\Documents\MATLAB\2013\01\2013_01_21\ElementStructureAllBirds_8.mat')
+%%
+addpath('C:\Users\Eathan\Documents\MATLAB\2013\01\2013_01_21\Functions')
+%%
 birdNames = {'Bayliss', 'Beasley', 'HT77', 'HT99', 'Lumsdaine', 'Powys', 'Johnson1', 'Lumsdaine2'};
 
 
