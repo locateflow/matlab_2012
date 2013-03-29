d49bae538d6b8f58c21bd0d2906924cb1f0d24d5
Eathan <Eathan@EATHAN-PC>
2013-03-29 11:16:34 -0400

    I finally have CV data for the new approach to markov model as well as similar data for the permuted markov model.

--- DailyCommandsLog.m
+++ DailyCommandsLog.m
@@ -1,79 +1,60 @@
-shuffled_AvoidRep_PhraseMarkov_Orig_ELEMENTS = zeros(4, 8);
-shuffled_AvoidRep_PhraseMarkov_Orig_PHRASES = zeros(4, 8);
-% markov data:
-load('C:\Users\Eathan\Documents\MATLAB\2012\2013\02\shuffledMarkovEtc.mat', 'allBirdsCVElementPhrase')
-%%
-shuffled_AvoidRep_PhraseMarkov_Orig_ELEMENTS(3,:) = allBirdsCVElementPhrase.phraseMarkov(1,:);
-shuffled_AvoidRep_PhraseMarkov_Orig_PHRASES(3,:) = allBirdsCVElementPhrase.phraseMarkov(2,:);
-%%
-load('C:\Users\Eathan\Documents\MATLAB\2012\2013\02\2013_02_13\shuffledCVcompareOrig.mat', 'BaylissShuffledOrigCVs')
-load('C:\Users\Eathan\Documents\MATLAB\2012\2013\02\2013_02_13\shuffledCVcompareOrig.mat', 'BeasleyShuffledOrigCVs')
-load('C:\Users\Eathan\Documents\MATLAB\2012\2013\02\2013_02_13\shuffledCVcompareOrig.mat', 'HT77ShuffledOrigCVs')
-load('C:\Users\Eathan\Documents\MATLAB\2012\2013\02\2013_02_13\shuffledCVcompareOrig.mat', 'LumsdaineShuffledOrigCVs')
-load('C:\Users\Eathan\Documents\MATLAB\2012\2013\02\2013_02_13\shuffledCVcompareOrig.mat', 'PowysShuffledOrigCVs')
-load('C:\Users\Eathan\Documents\MATLAB\2012\2013\02\2013_02_13\shuffledCVcompareOrig.mat', 'Johnson1ShuffledOrigCVs')
-load('C:\Users\Eathan\Documents\MATLAB\2012\2013\02\2013_02_13\shuffledCVcompareOrig.mat', 'Lumsdaine2ShuffledOrigCVs')
-load('C:\Users\Eathan\Documents\MATLAB\2012\2013\02\2013_02_13\shuffledCVcompareOrig.mat', 'HT99ShuffledOrigCVs')
-%%
-% birdNames = {'Bayliss', 'Beasley', 'HT77', 'HT99', 'Lumsdaine', 'Powys', 'Johnson2', 'Lumsdaine2'};
-shuffled_AvoidRep_PhraseMarkov_Orig_ELEMENTS([1,4],1) = BaylissShuffledOrigCVs(:, 1);
-shuffled_AvoidRep_PhraseMarkov_Orig_ELEMENTS([1,4],2) = BeasleyShuffledOrigCVs(:, 1);
-shuffled_AvoidRep_PhraseMarkov_Orig_ELEMENTS([1,4],3) = HT77ShuffledOrigCVs(:, 1);
-shuffled_AvoidRep_PhraseMarkov_Orig_ELEMENTS([1,4],4) = HT99ShuffledOrigCVs(:, 1);
-shuffled_AvoidRep_PhraseMarkov_Orig_ELEMENTS([1,4],5) = LumsdaineShuffledOrigCVs(:, 1);
-shuffled_AvoidRep_PhraseMarkov_Orig_ELEMENTS([1,4],6) = PowysShuffledOrigCVs(:, 1);
-shuffled_AvoidRep_PhraseMarkov_Orig_ELEMENTS([1,4],7) = Johnson1ShuffledOrigCVs(:, 1);
-shuffled_AvoidRep_PhraseMarkov_Orig_ELEMENTS([1,4],8) = Lumsdaine2ShuffledOrigCVs(:, 1);
-%%
-% shuffled_AvoidRep_PhraseMarkov_Orig_ELEMENTS = shuffled_AvoidRep_PhraseMarkov_Orig_ELEMENTS([2,2,3,4], :);
-%%
-shuffled_AvoidRep_PhraseMarkov_Orig_PHRASES([1,4],1) = BaylissShuffledOrigCVs(:, 2);
-shuffled_AvoidRep_PhraseMarkov_Orig_PHRASES([1,4],2) = BeasleyShuffledOrigCVs(:, 2);
-shuffled_AvoidRep_PhraseMarkov_Orig_PHRASES([1,4],3) = HT77ShuffledOrigCVs(:, 2);
-shuffled_AvoidRep_PhraseMarkov_Orig_PHRASES([1,4],4) = HT99ShuffledOrigCVs(:, 2);
-shuffled_AvoidRep_PhraseMarkov_Orig_PHRASES([1,4],5) = LumsdaineShuffledOrigCVs(:, 2);
-shuffled_AvoidRep_PhraseMarkov_Orig_PHRASES([1,4],6) = PowysShuffledOrigCVs(:, 2);
-shuffled_AvoidRep_PhraseMarkov_Orig_PHRASES([1,4],7) = Johnson1ShuffledOrigCVs(:, 2);
-shuffled_AvoidRep_PhraseMarkov_Orig_PHRASES([1,4],8) = Lumsdaine2ShuffledOrigCVs(:, 2);
-%% Run March_20_2013.m to get reinsert data
-%save('C:\Users\Eathan\Documents\MATLAB\2013\03\2013_03_20\todaysDataWithReinsertRepeat.mat')
-shuffled_AvoidRep_PhraseMarkov_Orig_ELEMENTS(2,1) = BaylissShuffledOrigCVs(1, 1);
-shuffled_AvoidRep_PhraseMarkov_Orig_ELEMENTS(2,2) = BeasleyShuffledOrigCVs(1, 1);
-shuffled_AvoidRep_PhraseMarkov_Orig_ELEMENTS(2,3) = HT77ShuffledOrigCVs(1, 1);
-shuffled_AvoidRep_PhraseMarkov_Orig_ELEMENTS(2,4) = HT99ShuffledOrigCVs(1, 1);
-shuffled_AvoidRep_PhraseMarkov_Orig_ELEMENTS(2,5) = LumsdaineShuffledOrigCVs(1, 1);
-shuffled_AvoidRep_PhraseMarkov_Orig_ELEMENTS(2,6) = PowysShuffledOrigCVs(1, 1);
-shuffled_AvoidRep_PhraseMarkov_Orig_ELEMENTS(2,7) = Johnson1ShuffledOrigCVs(1, 1);
-shuffled_AvoidRep_PhraseMarkov_Orig_ELEMENTS(2,8) = Lumsdaine2ShuffledOrigCVs(1, 1);
-%%
-% shuffled_AvoidRep_PhraseMarkov_Orig_ELEMENTS = shuffled_AvoidRep_PhraseMarkov_Orig_ELEMENTS([2,2,3,4], :);
-%%
-shuffled_AvoidRep_PhraseMarkov_Orig_PHRASES(2,1) = BaylissShuffledOrigCVs(1, 2);
-shuffled_AvoidRep_PhraseMarkov_Orig_PHRASES(2,2) = BeasleyShuffledOrigCVs(1, 2);
-shuffled_AvoidRep_PhraseMarkov_Orig_PHRASES(2,3) = HT77ShuffledOrigCVs(1, 2);
-shuffled_AvoidRep_PhraseMarkov_Orig_PHRASES(2,4) = HT99ShuffledOrigCVs(1, 2);
-shuffled_AvoidRep_PhraseMarkov_Orig_PHRASES(2,5) = LumsdaineShuffledOrigCVs(1, 2);
-shuffled_AvoidRep_PhraseMarkov_Orig_PHRASES(2,6) = PowysShuffledOrigCVs(1, 2);
-shuffled_AvoidRep_PhraseMarkov_Orig_PHRASES(2,7) = Johnson1ShuffledOrigCVs(1, 2);
-shuffled_AvoidRep_PhraseMarkov_Orig_PHRASES(2,8) = Lumsdaine2ShuffledOrigCVs(1, 2);
-%%
-birdNames = {'Bayliss', 'Beasley', 'HT77', 'HT99', 'Lumsdaine', 'Powys', 'Johnson2', 'Lumsdaine2'};
+load('C:\Users\Eathan\Documents\MATLAB\2013\01\2013_01_21\ElementStructureAllBirds_8.mat')
+addpath('2012\10\Functions')
+addpath('2013\01\2013_01_21\Functions')
 
-plot(shuffled_AvoidRep_PhraseMarkov_Orig_ELEMENTS, '-o')
-title('elements CV')
-xlim([0, 5])
-set(gca, 'xticklabel', {'', 'Suffled', 'Avoid Repeat', 'Markov', 'Original'})
-legend(birdNames)
+birdNames = {'Bayliss', 'Beasley', 'HT77', 'HT99', 'Lumsdaine', 'Powys', 'Johnson1', 'Lumsdaine2'};
+for j = 1:length(birdNames)
+    mat = eval(cell2mat(birdNames(j)));
+    len = length(mat(:, 1));  
+    
+        inputModel = mat;
+        inputModel(isnan(inputModel)) = -77;
+        uPhrase = unique(inputModel, 'rows');
+        r = rand(length(uPhrase(:, 1)),1);
+        ru = [r, uPhrase];
+        ru = sortrows(ru, 1);
+        newPhrase = ru(:,2:end);
+       
+for i = 1:100   
+%     r = rand(len, 1); 
+    markovModel = makeMarkovPhraseModelFromElementMat(mat);
+    elementIPIs = getElementIPIs(markovModel);
+    phraseIPIs = getPhraseIPIs(markovModel);
+    ele = elementIPIs.allDiffsNormalized;
+    phr = phraseIPIs.allDiffsNormalized;
+    CVs(i,1:2) = [std(ele)/mean(ele), std(phr)/mean(phr)];
+        
+        for ii = 1:length(uPhrase)
+        inputModel = markovModel;
+        inputModel(isnan(inputModel)) = -77;
+        f = find(ismember(inputModel, uPhrase(ii, :), 'rows'));
+        repmat(newPhrase(ii, :), length(f), 1);
+        randomizedPhraseMarkovModel(f,:) = repmat(newPhrase(ii, :), length(f), 1);       
+        end
+        randomizedPhraseMarkovModel(randomizedPhraseMarkovModel == -77) = NaN;
+        %        
+        elementIPIs = getElementIPIs(randomizedPhraseMarkovModel);
+        phraseIPIs = getPhraseIPIs(randomizedPhraseMarkovModel);
+        ele = elementIPIs.allDiffsNormalized;
+        phr = phraseIPIs.allDiffsNormalized;
+        permuteMarkovCVs(i,1:2) = [std(ele)/mean(ele), std(phr)/mean(phr)];        
+       
+end
+    mnCVs = mean(CVs);
+    meanCVsElePhrMarkovModel(j,:) = mnCVs;
+    mnPermuteMarkovCVs = mean(permuteMarkovCVs);
+    meanCVsElePhrPermuteMarkov(j,:) = mnPermuteMarkovCVs;
+    clear randomizedPhraseMarkovModel
+end
+
+%% NEXT step: add permute markov models
 figure
-plot(shuffled_AvoidRep_PhraseMarkov_Orig_PHRASES, '-o')
-title('phrases CV')
-xlim([0, 5])
-set(gca, 'xticklabel', {'', 'Suffled', 'Avoid Repeat', 'Markov', 'Original'})
-legend(birdNames)
-%%
-
-
-
-
-
-
+plot(meanCVsElePhrMarkovModel','o-')
+xlim([0 3])
+ylim([0 1])
+title('markov')
+figure
+plot(meanCVsElePhrPermuteMarkov','o-')
+xlim([0 3])
+ylim([0 1])
+title('permute')
