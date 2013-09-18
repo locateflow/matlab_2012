fcee29acf4d99d0e1b6252332bdadad822cdccd6
Eathan <Eathan@EATHAN-PC>
2013-09-18 13:47:19 -0400

    copied commands log from apr 4 2013 to work on adding another butcherbird's data

--- DailyCommandsLog.m
+++ DailyCommandsLog.m
@@ -1,105 +1,83 @@
-% opened handsorted_in-process_newFunctions2 from 2013_06_17
-open('C:\Users\Eathan\Documents\MATLAB\2013\06\2013_06_17\handsorted_in-process_newFunctions2.fig')
+% copied commands log from apr 4 2013 to work on adding another
+% butcherbird's data
+load('C:\Users\Eathan\Documents\MATLAB\2013\01\2013_01_21\ElementStructureAllBirds_8.mat')
 %%
-set(gca, 'clim', [1200 1500])
+addpath('2012\10\Functions')
+addpath('2013\01\2013_01_21\Functions')
+
+birdNames = {'Bayliss', 'Beasley', 'HT77', 'HT99', 'Lumsdaine', 'Powys', 'Johnson1', 'Lumsdaine2'};
+for j = 1:length(birdNames)
+    mat = eval(cell2mat(birdNames(j)));
+    len = length(mat(:, 1));  
+    
+        inputModel = mat;
+        inputModel(isnan(inputModel)) = -77;
+        uPhrase = unique(inputModel, 'rows');
+%         permutations = perms(1:length(uPhrase(:, 1)));
+        permutations = rand(100, length(uPhrase(:, 1)));
+for jj = 1:length(permutations(:, 1))
+        thisPerm = permutations(jj, :)';
+        thisPermU = [thisPerm, uPhrase];
+        thisPermU = sortrows(thisPermU, 1);
+        newPhrase = thisPermU(:,2:end);
+       
+for i = 1:100   
+%     r = rand(len, 1); 
+    markovModel = makeMarkovPhraseModelFromElementMat(mat);
+%     elementIPIs = getElementIPIs(markovModel);
+%     phraseIPIs = getPhraseIPIs(markovModel);
+%     ele = elementIPIs.allDiffsNormalized;
+%     phr = phraseIPIs.allDiffsNormalized;
+%     CVs(i,1:2) = [std(ele)/mean(ele), std(phr)/mean(phr)];
+        
+        for ii = 1:length(uPhrase)
+        inputModel = markovModel;
+        inputModel(isnan(inputModel)) = -77;
+        f = find(ismember(inputModel, uPhrase(ii, :), 'rows'));
+        repmat(newPhrase(ii, :), length(f), 1);
+        randomizedPhraseMarkovModel(f,:) = repmat(newPhrase(ii, :), length(f), 1);       
+        end
+        randomizedPhraseMarkovModel(randomizedPhraseMarkovModel == -77) = NaN;
+        %        
+        elementIPIs = getElementIPIs(randomizedPhraseMarkovModel);
+        phraseIPIs = getPhraseIPIs(randomizedPhraseMarkovModel);
+        ele = elementIPIs.allDiffsNormalized;
+        phr = phraseIPIs.allDiffsNormalized;
+        permuteMarkovCVs(i,1:2) = [std(ele)/mean(ele), std(phr)/mean(phr)];        
+       
+end
+%     mnCVs = mean(CVs);
+%     meanCVsElePhrMarkovModel(j,:) = mnCVs;
+    mnPermuteMarkovCVs = mean(permuteMarkovCVs);
+    meanCVsElePhrPermuteMarkov(j,:,jj) = mnPermuteMarkovCVs;
+    clear randomizedPhraseMarkovModel
+end
+end
+%%
+clear all
+load('C:\Users\Eathan\Documents\MATLAB\2013\04\2013_04_04\distributionOfPermutedMarkovs.mat')
+load('C:\Users\Eathan\Documents\MATLAB\2013\03\2013_03_29\CVsElePhrMarkovAndPermute.mat', 'meanCVsElePhrMarkovModel')
 
 %%
-set(gca, 'clim', [1300 1750])
-%%
-set(gca, 'clim', [800 2500])
-%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-set(gca, 'clim', [500 2500])
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%
-set(gca, 'clim', [400 1200])
-%%
-set(gca, 'clim', [1000 1300])
-%%
-set(gca, 'clim', [1200 1500])
-%%
-set(gca, 'clim', [700 1200])
-%%
-set(gca, 'clim', [975 1075])
-%%
-set(gca, 'clim', [950 1250])
-%%
-set(gca, 'clim', [500 1200])
-%%
-set(gca, 'clim', [1200 1400])
-%%
-set(gca, 'clim', [1300 2000])
-%%
-set(gca, 'clim', [950 1300])
-%%
-set(gca, 'clim', [1300 2400])
-%%
-set(gca, 'clim', [800 1400])
-%%
-set(gca, 'clim', [900 1450])
-%%
-set(gca, 'clim', [500 1300])
-%%
-set(gca, 'clim', [1050 1250])
-%%
-set(gca, 'clim', [1500 2500])
-%%
-set(gca, 'clim', [1800 2500])
-%%
-set(gca, 'clim', [850 1000])
-%%
-set(gca, 'clim', [1100 1300])
-%%
-set(gca, 'clim', [800 1200])
-%%
-set(gca, 'clim', [900 1150])
-%%
-set(gca, 'clim', [700 1000])
-%%
-set(gca, 'clim', [800 1300])
-%%
-set(gca, 'clim', [900 1400])
-%%
-set(gca, 'clim', [900 1200])
-%%
-set(gca, 'clim', [1100 1300])
-%%
-set(gca, 'clim', [1100 1500])
-%%
-set(gca, 'clim', [1000 2000])
-%%
-set(gca, 'clim', [1500 2000])
-%%
-set(gca, 'clim', [1100 1300])
-%%
-set(gca, 'clim', [1300 1700])
-%%
-set(gca, 'clim', [750 1250])
-%%
-set(gca, 'clim', [800 1200])
-%%
-set(gca, 'clim', [800 1200])
-%%
-set(gca, 'clim', [1500 2500])
-%%
-set(gca, 'clim', [2000 2500])
-%%
-set(gca, 'clim', [1500 2000])
-%%
-set(gca, 'clim', [1000 2000])
-%%
-set(gca, 'clim', [500 1500])
-%%
-set(gca, 'clim', [950 1200])
-%%
-set(gca, 'clim', [1150 1750])
-%%
-set(gca, 'clim', [500 1500])
-%%
-set(gca, 'clim', [1200 1350])
-%%
-set(gca, 'clim', [1000 1300])
-%%
-set(gca, 'clim', [1700 2500])
-%%
-set(gca, 'clim', [500 1200])
+
+
+% for i = 1:8
+figure
+subplot(131)
+    plot(1:100, sort(squeeze(meanCVsElePhrPermuteMarkov(:, 1, :))'));
+    ylim([.3 .9])
+    title('Phrase Markov Permutation (Elements)')
+    ylabel('CV')
+subplot(132)
+    plot(meanCVsElePhrMarkovModel', '-o')
+    ylim([.3 .9])
+    xlim([0 3])
+    title('"Chosen" Markov Model')
+subplot(133)
+    plot(1:100, sort(squeeze(meanCVsElePhrPermuteMarkov(:, 2, :))'));
+    ylim([.3 .9])
+    title('Phrase Markov Permutation (phrases)')
+
+%     plot(load('C:\Users\Eathan\Documents\MATLAB\2013\03\2013_03_29\CVsElePhrMarkovAndPermute.mat')
+
+% end
